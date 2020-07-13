73d5f4c551dab1f643ae27969b666f17f5c15275
Emmanuel Aweperi Adiba <eabaagah@gmail.com>
2020-07-13 12:22:36 +0000

    Created database class

--- config/init.class.php
+++ config/init.class.php
@@ -4,7 +4,7 @@ require_once 'config.class.php';
 spl_autoload_register ('myAutoloader');
 
 function myAutoloader($classname) {
-    $url = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+    $url = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
         $path = 'lib/';
         $extention = '.class.php';
